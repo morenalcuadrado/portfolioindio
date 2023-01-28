bbd03970e3f2ecfc7316ee93fba9141f94cfc874
morenalcuadrado <dalaiaslargo@gmail.com>
2023-01-28 20:02:58 -0300

    propiedades TWD en NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import NavBar from "./components/NavBar";
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">Hello what you doing</h1>
+      <NavBar />
     </div>
   );
 }
