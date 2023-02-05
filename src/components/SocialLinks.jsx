15659247ab482260f4de7b9ea1ff286f90cd009c
morenalcuadrado <dalaiaslargo@gmail.com>
2023-02-05 18:24:30 -0300

    termino de Social Links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -24,7 +24,6 @@ const SocialLinks = () => {
             </>
             ),
             href: 'https://github.com/morenalcuadrado',
-            style: 'rounded-tr-md'
         },
         {
             id: 3,
@@ -33,8 +32,7 @@ const SocialLinks = () => {
                 Gmail <HiOutlineMail size={30}/>
             </>
             ),
-            href: 'Mailto:dalaiaslargol@gmail.com',
-            style: 'rounded-tr-md'
+            href: 'mailto:dalaiaslargol@gmail.com',
         },
         {
             id: 4,
@@ -51,18 +49,17 @@ const SocialLinks = () => {
     ]
 
 return (
-    <div className='Flex-col top-[35%] left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 '>
-                <a href className='flex justify-between items-center w-full text-white'>
-                    <>
-                        Linkedin <FaLinkedin size={30}/>
-                    </>
+            {links.map(({ id, child, href, style, download }) => (
+                <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " + style}>
+                <a href={href} className='flex justify-between items-center w-full text-white' download={download} target="_blank" rel="noreferrer">
+                    {child}
                 </a>
             </li>
+            ))}
         </ul>
     </div>
 );
 };
-
 export default SocialLinks;
