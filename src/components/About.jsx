39d217de307f897edb36d00fcfa4ccd3ce0e2401
morenalcuadrado <dalaiaslargo@gmail.com>
2023-02-05 18:52:13 -0300

    componete About

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold'>About</p>
+            </div>
+                <p className='text-xl mt-5'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas fuga dolorum, dolorem, sint incidunt quibusdam est esse magnam pariatur a beatae. Eum consequatur neque unde totam delectus nulla at similique temporibus laboriosam? Iure ad possimus officiis ab aut, doloribus ut suscipit officia sequi, perspiciatis vitae minus. Repudiandae blanditiis dolore fugit?
+                </p>
+                <br />
+                <p className='text-xl'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas fuga dolorum, dolorem, sint incidunt quibusdam est esse magnam pariatur a beatae. Eum consequatur neque unde totam delectus nulla at similique temporibus laboriosam? Iure ad possimus officiis ab aut, doloribus ut suscipit officia sequi, perspiciatis vitae minus. Repudiandae blanditiis dolore fugit?
+                </p>
+        </div>
+    </div>
+)
+}
+
+export default About;
